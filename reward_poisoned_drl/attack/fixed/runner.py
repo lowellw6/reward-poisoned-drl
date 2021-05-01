d86e54847d2473856d3f01104f968aabdfe07bd7
Lowell Weissman <lowell6@vt.edu>
2021-05-01 13:20:55 -0400

    fixed indentation

--- reward_poisoned_drl/attack/fixed/runner.py
+++ reward_poisoned_drl/attack/fixed/runner.py
@@ -13,17 +13,17 @@ class MinibatchRlLogNotify(MinibatchRl):
     """
 
     def train(self):
-            n_itr = self.startup()
-            for itr in range(n_itr):
-                logger.set_iteration(itr)
-                logging_this_itr = (itr + 1) % self.log_interval_itrs == 0
-                with logger.prefix(f"itr #{itr} "):
-                    self.agent.sample_mode(itr)  # Might not be this agent sampling.
-                    samples, traj_infos = self.sampler.obtain_samples(itr)
-                    self.agent.train_mode(itr)
-                    self.algo.log_notify(logging_this_itr)
-                    opt_info = self.algo.optimize_agent(itr, samples)
-                    self.store_diagnostics(itr, traj_infos, opt_info)
-                    if logging_this_itr:
-                        self.log_diagnostics(itr)
-            self.shutdown()
+        n_itr = self.startup()
+        for itr in range(n_itr):
+            logger.set_iteration(itr)
+            logging_this_itr = (itr + 1) % self.log_interval_itrs == 0
+            with logger.prefix(f"itr #{itr} "):
+                self.agent.sample_mode(itr)  # Might not be this agent sampling.
+                samples, traj_infos = self.sampler.obtain_samples(itr)
+                self.agent.train_mode(itr)
+                self.algo.log_notify(logging_this_itr)
+                opt_info = self.algo.optimize_agent(itr, samples)
+                self.store_diagnostics(itr, traj_infos, opt_info)
+                if logging_this_itr:
+                    self.log_diagnostics(itr)
+        self.shutdown()
